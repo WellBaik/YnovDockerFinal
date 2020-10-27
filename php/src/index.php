bff5d8e1aa0abada7a2a846b9c2bf76bda1a2ac8
WellBaik <benj-f@sfr.fr>
2020-10-27 20:47:07 +0100

    began php-mysql request

--- php/src/index.php
+++ php/src/index.php
@@ -1,2 +1,31 @@
 <?php
-echo $_SERVER['REQUEST_URI'];
+$txt = $_SERVER['REQUEST_URI'];
+$pos = strrpos($txt,"/");
+echo $pos;
+echo "<br>";
+$name = substr($txt, $pos+1);
+echo $name;
+echo "<br>";
+echo strlen($name);
+
+if(strlen($name)>0){
+    echo "Un nom est présent dans l'url";
+    $servername = "localhost";
+    $username = "user";
+    $password = "pass";
+    $dbname = "db";
+    $port = "3306";
+    
+    try {
+        $dbh = new PDO('mysql:host=localhost;port=$port;dbname=db', $user, $pass);
+        foreach($dbh->query('SELECT * from name') as $row) {
+            print_r($row);
+        }
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    } 
+} else {
+    echo "Aucun nom dans l'url";
+}
