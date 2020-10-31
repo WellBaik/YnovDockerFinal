0e6a7a9cf3cf17a8b58f4dfc77ed13d75b3e6288
WellBaik <benj-f@sfr.fr>
2020-10-31 15:15:21 +0100

    php requete ajout name

--- php/src/index.php
+++ php/src/index.php
@@ -1,31 +1,32 @@
 <?php
 $txt = $_SERVER['REQUEST_URI'];
-$pos = strrpos($txt,"/");
-echo $pos;
-echo "<br>";
-$name = substr($txt, $pos+1);
-echo $name;
-echo "<br>";
-echo strlen($name);
+$pos = strrpos($txt,"/put_name/");
+$name = substr($txt, $pos+10);
+if($pos>=10){
+    if(strlen($name)>0){
+        $servername = "localhost";
+        $username = "user";
+        $password = "pass";
+        $dbname = "db";
+        $port = "3306";
 
-if(strlen($name)>0){
-    echo "Un nom est présent dans l'url";
-    $servername = "localhost";
-    $username = "user";
-    $password = "pass";
-    $dbname = "db";
-    $port = "3306";
-    
-    try {
-        $dbh = new PDO('mysql:host=mysql;port=$port;dbname=db', $username, $password);
-        foreach($dbh->query('SELECT * from name') as $row) {
-            print_r($row);
-        }
-        $dbh = null;
-    } catch (PDOException $e) {
-        print "Erreur !: " . $e->getMessage() . "<br/>";
-        die();
-    } 
+        try {
+            $dbh = new PDO('mysql:host=mysql;port=$port;dbname=db', $username, $password);
+            $request = "INSERT INTO name (name) VALUES ('".$name."')";
+            $stmt = $dbh->prepare($request);     
+            $retour = $stmt->execute();
+            if($retour==1){
+                echo "Le nom a bien été ajouté";    
+            } else {
+                echo "Une erreur est survenue";
+            }
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        } 
+    } else {
+        echo "Aucun nom dans l'url";
+    }
 } else {
-    echo "Aucun nom dans l'url";
+    echo "Url invalide";
 }
